4fe54c624436cd51d1b6e1209ef9d07bf5cda6d0
sjuknelis <sjuknelis24@nobles.edu>
2025-04-17 20:55:00 -0400

    working atmel

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "../ASF/thirdparty/RTOS/freertos/FreeRTOSV10.0.0/Source/include/FreeRTOS.h"
 #include "../ASF/thirdparty/RTOS/freertos/FreeRTOSV10.0.0/Source/include/task.h"
+#include "../ASF/atmel_start.h"
+#include "../ASF/atmel_start_pins.h"
 
 // int main(void);
 
@@ -10,9 +12,16 @@ volatile int test = 8;
 int main(void) {
     while (test != 8);
 
+    atmel_start_init();
+
     volatile int a = 1;
     a++;
-    for (;;);
+    while (1) {
+        gpio_set_pin_level(LED_Orange1, false);
+        // for (int i = 0; i < 10000; i++);
+        gpio_set_pin_level(LED_Orange1, true);
+        // for (int i = 0; i < 10000; i++);
+    }
 }
 
 void vApplicationStackOverflowHook(TaskHandle_t xTask, signed char *pcTaskName) {
